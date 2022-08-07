1091eee727a1ff609da363f1d38106d6c228003e
UltramarinVos <ustugov1368@gmail.com>
2022-08-07 17:02:24 +0400

    work 1-25

---
+++ codebasicswork.php
@@ -0,0 +1,152 @@
+1
+<?php
+
+echo 'Hello, World!';?>
+
+2
+<?php
+
+print_r('King in the North!');?>
+
+3
+<?php
+
+// You know nothing, Jon Snow!.?>
+
+4
+<?php
+
+print_r('Robert');
+print_r('Stannis');
+print_r('Renly');?>
+
+5
+<?php
+
+echo '9780262531962';?>
+
+6
+<?php 
+
+print_r('What Is Dead May Never Die');?>
+
+7
+<?php
+
+print_r(81 / 9);?>
+
+8
+<?php
+
+print_r(6 - -81);?>
+
+9
+<?php
+
+print_r(3**5);
+print_r(-8/-4);?>
+
+10
+<?php
+
+print_r(8 / 2 + 5 - -3 / 2);?>
+
+11
+<?php
+
+print_r(70 * (3 + 4) / (8 + 2));?>
+
+12
+<?php
+
+print_r((5 ** 2) - (3 * 7));?>
+
+13
+<?php
+
+print_r('"Khal Drogo\'s favorite word is "athjahakar""');?>
+
+14
+<?php
+
+print_r("- Did Joffrey agree?\n- He did. He also said \"I love using \\n\".");?>
+
+15
+<?php
+
+print_r("Winter came for the House " . "of Frey.");?>
+
+16
+<?php
+
+print_r(chr(126));
+print_r(chr(94));
+print_r(chr(37));?>
+
+17
+<?php
+
+print_r(-0.304);?>
+
+18
+<?php
+
+print_r('7' -(-8 - -2));?>
+
+19
+<?php
+
+print_r((string) ((int) 2.9) . ' times');?>
+
+20
+<?php
+
+$motto = 'What Is Dead May Never Die!';
+print_r($motto);?>
+
+21
+<?php
+
+$name = 'Brienna';
+
+$name = 'anneirB';
+
+print_r($name);?>
+
+22
+<?php
+
+$numberofbrothers = '2';
+
+print_r($numberofbrothers);?>
+
+23
+<?php
+
+$family = 'Targaryen';
+
+$pet = 'Dragon';
+
+print_r($family);
+print_r(' and ');
+print_r($pet);?>
+
+24
+
+
+25
+
+
+26
+
+
+27
+
+
+28
+
+
+29
+
+
+30
